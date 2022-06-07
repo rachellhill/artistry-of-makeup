ccbb5392bad3419a1a38ed5ac8a655334a0ca596
Rachel Hill <hillrachel187@gmail.com>
2022-06-07 17:28:18 -0500

    Add file structure and fetch data with hooks

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Makeup from './Makeup';
+import '../styles/App.css';
+
+const App = () => {
+  const [makeup, setMakeup] = useState([])
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchData = async () => {
+    const url = 'http://makeup-api.herokuapp.com/api/v1/products.json'
+
+    try {
+      const response = await fetch(url)
+      const makeup = await response.json()
+      setMakeup(makeup)
+    } catch(error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }
+  
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+   <>
+
+   </>
+  );
+}
+
+export default App;
